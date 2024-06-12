0a829addef51a5e40d8bfb1e0a6e8016cab9123e
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2024-06-12 20:25:18 +0900

    Init: VisionTimer project

---
+++ VisionTimer/VisionTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisionTimer
+//
+//  Created by 이민규 on 6/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ VisionTimer/VisionTimer/VisionTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionTimerApp.swift
+//  VisionTimer
+//
+//  Created by 이민규 on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
